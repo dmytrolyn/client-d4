f613b2a098a372cac4b28f7bbb03d1f2735a78c2
DmytroLyn <dlin3738@gmail.com>
2024-11-26 21:58:58 +0200

    fix: build

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,10 +1,9 @@
 import { Box, Typography, CircularProgress } from "@mui/material";
 import useSWR from "swr";
 import { fetcher, infoUrl } from "@/api";
-import { Info } from "@/types";
 
 export const InfoPage = () => {
-  const { data } = useSWR<Info>(infoUrl, fetcher);
+  const { data } = useSWR(infoUrl, fetcher);
 
   if (!data) return <CircularProgress />;
 
